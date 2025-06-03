285a153903414bb4715d7238a7bc33e7dcb4719b
manaspratap1 <manassingh449@gmail.com>
2025-06-03 23:36:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
